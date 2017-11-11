d290ef6be3987b523f0a84e2064029f59c19ac48
andreasDL <andreasdelille@hotmail.com>
2017-11-11 15:21:36 +0100

    acronym

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -1,17 +1,18 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package acronym should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package acronym
 
-func Abbreviate(string) string
+import "strings"
 
-// Abbreviate should have a comment documenting it.
+
 func Abbreviate(s string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+
+	s = strings.Replace(s, "-", " ", -1)
+	f := strings.Fields(s)
+
+
+	out := ""
+	for _, c := range f {
+		out += strings.ToUpper(string(c[0]))
+	}
+
+	return out
 }
