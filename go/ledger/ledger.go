f1e8c419d0ffb7596f2bb84594321d8a25318331
andreasDL <andreasdelille@hotmail.com>
2017-12-24 17:18:05 +0100

    refactoring copy

--- go/ledger/ledger.go
+++ go/ledger/ledger.go
@@ -21,20 +21,22 @@ func FormatLedger(currency string, locale string, entries []Entry) (string, erro
 			return "", err
 		}
 	}
+
 	m1 := map[bool]int{true: 0, false: 1}
 	m2 := map[bool]int{true: -1, false: 1}
-
 	for i, first := range entriesCopy {
 		for j := i+1 ; j < len(entriesCopy) ; j++ {
 			e := entriesCopy[j]
 
-			if (m1[e.Date == first.Date]*m2[e.Date < first.Date]*4 +
-				m1[e.Description == first.Description]*m2[e.Description < first.Description]*2 +
-				m1[e.Change == first.Change]*m2[e.Change < first.Change]*1) < 0 {
+
+			if (m1[e.Date == first.Date]*m2[e.Date < first.Date] +
+				m1[e.Description == first.Description]*m2[e.Description < first.Description] +
+				m1[e.Change == first.Change]*m2[e.Change < first.Change]
+
+			) < 0 {
 				
 				entriesCopy[i], entriesCopy[j] = entriesCopy[j], entriesCopy[i]
 			}
-
 		}
 	}
 
