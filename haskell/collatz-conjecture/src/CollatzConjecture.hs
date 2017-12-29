648325c879319d08acaf49d028ba9dd08c65c55d
andreasDL <andreasdelille@hotmail.com>
2017-12-29 22:20:14 +0100

    clean

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,4 @@
-module CollatzConjecture (collatz, main) where
+module CollatzConjecture (collatz) where
 
 import Data.Maybe (isNothing, fromJust)
 
@@ -6,54 +6,11 @@ collatz :: Integer -> Maybe Integer
 collatz x
         | x <= 0 = Nothing
         | x == 1 = Just 0
-        | otherwise = Just (1 + (fromJust val))
+        | otherwise = Just $ 1 + (fromJust $ collatz y)
     where 
         y = next x
-        val = collatz $ next x
 
 next :: Integer -> Integer
 next x 
     | even x = x `quot` 2
     | odd x  = (3*x)+1
-
-main = do 
-    putStrLn $ show $ collatz 12
-
-{-|
-collatz :: Integer -> Maybe Integer
-collatz x
-  | x <= 0    = Nothing
-  | otherwise = Just . fromIntegral . length . takeWhile (> 1) $ sequenceFrom x
-
-sequenceFrom :: Integer -> [Integer]
-sequenceFrom x = x:sequenceFrom(next x)
-
-next :: Integer -> Integer
-next x
-  | x `mod` 2 == 0 = x `div` 2
-  | otherwise  = (x * 3) + 1
-
-Take any positive integer n. If n is even, divide n by 2 to get n / 2. If n is
-odd, multiply n by 3 and add 1 to get 3n + 1. Repeat the process indefinitely.
-The conjecture states that no matter which number you start with, you will
-always reach 1 eventually.
-
-Given a number n, return the number of steps required to reach 1.
-
-## Examples
-
-Starting with n = 12, the steps would be as follows:
-
-0. 12
-1. 6
-2. 3
-3. 10
-4. 5
-5. 16
-6. 8
-7. 4
-8. 2
-9. 1
-
-Resulting in 9 steps. So for input n = 12, the return value would be 9.
-|-}
