f150b2e5296378ef6fdfc955f7c48adb15297b6c
andreasDL <andreasdelille@hotmail.com>
2017-12-30 12:14:24 +0100

    single line

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -3,7 +3,4 @@ module Pangram (isPangram) where
 import Data.Char (toLower)
 
 isPangram :: String -> Bool
-isPangram text = length charsFound == 26 
-    where
-        bools = map (`elem` (map toLower text)) ['a'..'z']
-        charsFound = filter id bools
+isPangram text = all (`elem` (map toLower text)) ['a'..'z']
