d7f4edd1171784d54b3d570befa77dc4d654db24
Andreas De Lille <andreasdelille@hotmail.com>
2018-01-04 14:39:43 +0100

    cleanup

--- haskell/acronym/src/Acronym.hs
+++ haskell/acronym/src/Acronym.hs
@@ -5,14 +5,10 @@ import Data.Char (isUpper, toUpper, isSpace, isAlpha)
 abbreviate :: String -> String
 abbreviate "" = ""
 abbreviate (x:y:xs)
-    | isSep x   && isAlpha y = toUpper y : abbreviate xs --Spaces or hyphens
+    | x `elem` "- "  && isAlpha y = toUpper y : abbreviate xs --Spaces or hyphens
     | isUpper x && isUpper y = abbreviate (x:xs) --throw out Y
     | isUpper x              = x:abbreviate (xs)
     | otherwise              = abbreviate (y:xs)
 abbreviate (x:xs)
     | isUpper x              = x:abbreviate xs
     | otherwise              = abbreviate xs
-
-
-isSep :: Char -> Bool
-isSep c = isSpace c || c `elem` "-"
