a21f999216db7d37789b5f9a2157742a64c4fb98
Andreas De Lille <andreasdelille@hotmail.com>
2018-01-04 15:18:54 +0100

    finish

--- haskell/grade-school/src/School.hs
+++ haskell/grade-school/src/School.hs
@@ -1,15 +1,20 @@
 module School (School, add, empty, grade, sorted) where
 
-data School = Dummy
+import qualified Data.Map as M
+import Data.List (sort)
+
+type School = M.Map Int [String]
 
 add :: Int -> String -> School -> School
-add gradeNum student school = error "You need to implement this function."
+add gradeNum student school 
+    | not $ M.member gradeNum school = M.insert gradeNum [student] school
+    | otherwise                      = M.update (\list -> Just $ sort $ list ++ [student]) gradeNum school
 
 empty :: School
-empty = error "You need to implement this function."
+empty = M.empty
 
 grade :: Int -> School -> [String]
-grade gradeNum school = error "You need to implement this function."
+grade gradeNum school = M.findWithDefault [] gradeNum school
 
 sorted :: School -> [(Int, [String])]
-sorted school = error "You need to implement this function."
+sorted = M.toAscList
